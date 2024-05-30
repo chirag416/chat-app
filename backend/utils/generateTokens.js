9ebae3f751c63fc1bd648caee684bb4926fb90be
Chirag Yadav <chiragyadav416@gmail.com>
2024-05-31 03:01:46 +0530

    socket url updated

--- backend/utils/generateTokens.js
+++ backend/utils/generateTokens.js
@@ -9,7 +9,7 @@ const generateTokenAndSetCookie = (userId, res) => {
         maxAge: 15 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: "strict",
-        secure: process.env.NODE_ENV !== "development",
+        secure: true,
     });
 };
 
